7248e45b2250e592918628e4f4a7e3eb0cb9ac53
Ruben Kober <ruben.kober@pm.me>
2025-04-24 01:01:12 +0200

    Added blog page and hid link to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {LandingPageComponent} from './components/pages/landing-page/landing-page.component';
+import { HomePageComponent } from './components/pages/blog/home-page/home-page.component';
 
 export const routes: Routes = [
-  {path: "", component: LandingPageComponent}
+  {path: "", component: LandingPageComponent},
+  {path: "blog", component: HomePageComponent}
 ];
